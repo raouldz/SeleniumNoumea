c25bdd969c84582ba224e72f31fcc0a2f2e82873
Raoul <dzonou@yahoo.com>
2023-12-18 12:12:19 +0100

    selenium project test1

--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -5,7 +5,7 @@ import main.java.pages.SecureAreaPage;
 import org.testng.annotations.Test;
 import test.java.base.BaseTests;
 
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 public class LoginTests extends BaseTests {
     @Test
@@ -14,6 +14,7 @@ public class LoginTests extends BaseTests {
         loginPage.setUsername("tomsmith");
         loginPage.setPassword("SuperSecretPassword!");
         SecureAreaPage SecureAreaLogin = loginPage.clickOnloginButton();
-        assertTrue(SecureAreaLogin.getAlertText().contains("You logged into a secure area! "),"Alert is incorrect");
+        assertTrue(SecureAreaLogin.getAlertText()
+                .contains("You logged into a secure area!"),"Alert is incorrect");
     }
 }
